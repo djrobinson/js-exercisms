a346f59d52b523f48b4f1d1f57ec419c5cb0fc45
Danny Robinson <daniel@djrobinson.me>
2016-02-22 12:38:14 -0700

    Completed robot name

--- robot-name/robot-name.js
+++ robot-name/robot-name.js
@@ -1,5 +1,28 @@
 var robotname = function(){
-  this.name = name;
+  this.name = newName();
+  this.reset = function(){
+    this.name = newName();
+  };
 }
 
-module.exports = robtoname;
+var i = 100;
+var a = 0;
+var b = 0;
+var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+function newName(){
+  if (i >= 999){
+    i = 0;
+  } else {
+    i++;
+  }
+
+  if (a >= 25){
+    a = 0;
+    b++;
+  } else {
+    a++;
+  }
+  return alphabet[a] + alphabet[a] + i;
+}
+
+module.exports = robotname;
